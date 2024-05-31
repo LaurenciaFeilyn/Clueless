d32c571fd73b19c6fdb7876b738ed569e6225cb9
Laurencia Feilyn <laurencia.ken@gmail.com>
2024-05-31 16:40:31 +0700

    Initial Commit

---
+++ Clueless/CluelessApp.swift
@@ -0,0 +1,17 @@
+//
+//  CluelessApp.swift
+//  Clueless
+//
+//  Created by Laurencia Feilyn on 31/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CluelessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
